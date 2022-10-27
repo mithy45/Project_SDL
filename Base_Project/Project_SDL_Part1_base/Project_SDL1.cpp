84325f86e01e0b8bd31c59c98b48d21ab190c6ec
quentin1.guyot <quentin1.guyot@epita.fr>
2022-10-27 16:24:03 +0200

    Init

---
+++ Base_Project/Project_SDL_Part1_base/Project_SDL1.cpp
@@ -0,0 +1,50 @@
+﻿// SDL_Test.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Project_SDL1.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <numeric>
+#include <random>
+#include <string>
+
+Application::Application(unsigned n_sheep, unsigned n_wolf)
+{
+  window_ptr_ = SDL_CreateWindow("Project_SDL_1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, frame_width, frame_height, SDL_WINDOW_SHOWN);
+  if (!window_ptr_)
+    throw std::runtime_error("Application(): SDL_CreateWindow could create the window" + std::string(SDL_GetError()));
+  window_surface_ptr_ = SDL_GetWindowSurface(window_ptr_);
+  if (!window_surface_ptr_)
+    throw std::runtime_error("Application(): SDL_GetWindowSurface could not get the windows surface" + std::string(SDL_GetError()));
+}
+
+void init()
+{
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO) < 0)
+    throw std::runtime_error("init():" + std::string(SDL_GetError()));
+
+  // Initialize PNG loading
+  int imgFlags = IMG_INIT_PNG;
+  if (!(IMG_Init(imgFlags) & imgFlags))
+    throw std::runtime_error("init(): SDL_image could not initialize! "
+                             "SDL_image Error: " +
+                             std::string(IMG_GetError()));
+}
+
+namespace
+{
+  // Defining a namespace without a name -> Anonymous workspace
+  // Its purpose is to indicate to the compiler that everything
+  // inside of it is UNIQUELY used within this source file.
+
+  SDL_Surface *load_surface_for(const std::string &path,
+                                SDL_Surface *window_surface_ptr)
+  {
+
+    // Helper function to load a png for a specific surface
+    // See SDL_ConvertSurface
+  }
+} // namespace
---
+++ Base_Project/Project_SDL_Part1_base/Project_SDL1.h
@@ -0,0 +1,100 @@
+﻿// SDL_Test.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <vector>
+
+// Defintions
+constexpr double frame_rate = 60.0; // refresh rate
+constexpr double frame_time = 1. / frame_rate;
+constexpr unsigned frame_width = 1400; // Width of window in pixel
+constexpr unsigned frame_height = 900; // Height of window in pixel
+// Minimal distance of animals to the border
+// of the screen
+constexpr unsigned frame_boundary = 100;
+
+// Helper function to initialize SDL
+void init();
+
+class Animal {
+private:
+  SDL_Surface* window_surface_ptr_; // ptr to the surface on which we want the
+                                    // animal to be drawn, also non-owning
+  SDL_Surface* image_ptr_; // The texture of the sheep (the loaded image), use
+                           // load_surface_for
+  // todo: Attribute(s) to define its position
+public:
+  Animal(const std::string& file_path, SDL_Surface* window_surface_ptr){};
+  // todo: The constructor has to load the sdl_surface that corresponds to the
+  // texture
+  ~Animal(){}; // todo: Use the destructor to release memory and "clean up
+               // behind you"
+
+  void draw(){}; // todo: Draw the animal on the screen <-> window_surface_ptr.
+                 // Note that this function is not virtual, it does not depend
+                 // on the static type of the instance
+
+  virtual void move(){} = 0; // todo: Animals move around, but in a different
+                             // fashion depending on which type of animal
+};
+
+// Insert here:
+// class sheep, derived from animal
+class Sheep : public Animal {
+  // todo
+  // Ctor
+  // Dtor
+  // implement functions that are purely virtual in base class
+};
+
+// Insert here:
+// class wolf, derived from animal
+// Use only sheep at first. Once the application works
+// for sheep you can add the wolves
+
+// The "ground" on which all the animals live (like the std::vector
+// in the zoo example).
+class Ground {
+private:
+  // Attention, NON-OWNING ptr, again to the screen
+  SDL_Surface* window_surface_ptr_;
+
+  // Some attribute to store all the wolves and sheep
+  // here
+
+public:
+  Ground(SDL_Surface* window_surface_ptr); // todo: Ctor
+  ~Ground(){}; // todo: Dtor, again for clean up (if necessary)
+  void add_animal(Animal a); // todo: Add an animal
+  void update(); // todo: "refresh the screen": Move animals and draw them
+  // Possibly other methods, depends on your implementation
+};
+
+// The application class, which is in charge of generating the window
+class Application {
+private:
+  // The following are OWNING ptrs
+  SDL_Window* window_ptr_;
+  SDL_Surface* window_surface_ptr_;
+  SDL_Event window_event_;
+
+  // Other attributes here, for example an instance of ground
+
+public:
+  Application(unsigned n_sheep, unsigned n_wolf); // Ctor
+  ~Application();                                 // dtor
+
+  int loop(unsigned period); // main loop of the application.
+                             // this ensures that the screen is updated
+                             // at the correct rate.
+                             // See SDL_GetTicks() and SDL_Delay() to enforce a
+                             // duration the application should terminate after
+                             // 'period' seconds
+};
